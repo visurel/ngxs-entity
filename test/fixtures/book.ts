f6004efaa978be16d2d29a5c763ec94a8d45542c
visurel <martin@visurel.com>
2018-07-13 15:54:25 +0200

    test: add test

---
+++ test/fixtures/book.ts
@@ -0,0 +1,19 @@
+export interface BookModel {
+  id: string
+  title: string
+}
+
+export const AClockworkOrange: BookModel = {
+  id: 'aco',
+  title: 'A Clockwork Orange'
+}
+
+export const AnimalFarm: BookModel = {
+  id: 'af',
+  title: 'Animal Farm'
+}
+
+export const TheGreatGatsby: BookModel = {
+  id: 'tgg',
+  title: 'The Great Gatsby'
+}
